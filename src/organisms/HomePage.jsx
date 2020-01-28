7a77d85d99d5c7c4cbeda58f22197f282f33ef8a
Mark Artishuk <markyshuk@gmail.com>
2020-01-28 13:31:12 -0800

    Added temporary max width constraint

--- src/organisms/HomePage.jsx
+++ src/organisms/HomePage.jsx
@@ -4,14 +4,20 @@ import HomeSearchBar from "../atoms/HomeSearchBar";
 import HomeIntro from "../molecules/HomeIntro";
 import CategoryGrid from "../molecules/CategoryGrid";
 
+import styled from "styled-components";
+
+const Container = styled.div`
+  margin: 0px 16px;
+`;
+
 const HomePage = () => {
   return (
-    <div>
+    <Container>
       <HomeHeader />
       <HomeSearchBar />
       <HomeIntro />
       <CategoryGrid />
-    </div>
+    </Container>
   );
 };
 
