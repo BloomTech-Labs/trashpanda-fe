bb466b5f435b627b22372ada2aff4a76037bbd15
Mark Artishuk <markyshuk@gmail.com>
2020-01-28 12:16:31 -0800

    Transformed App component from class component to functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return <div className="App"></div>;
-  }
-}
+const App = () => {
+  return <div className="App"></div>;
+};
 
 export default App;
