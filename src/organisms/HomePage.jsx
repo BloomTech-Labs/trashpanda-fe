ca24dc25e4012a8e122415f453d735cdb6037d49
Mark Artishuk <markyshuk@gmail.com>
2020-01-28 13:06:56 -0800

    Added home intro description/content

--- src/organisms/HomePage.jsx
+++ src/organisms/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HomeHeader from "../atoms/HomeHeader";
 import HomeSearchBar from "../atoms/HomeSearchBar";
+import HomeIntro from "../molecules/HomeIntro";
 
 const HomePage = () => {
   return (
     <div>
       <HomeHeader />
       <HomeSearchBar />
+      <HomeIntro />
     </div>
   );
 };
