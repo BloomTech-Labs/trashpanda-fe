ca88dacc89078129629eecafc71192c4f5a16893
Mark Artishuk <markyshuk@gmail.com>
2020-01-29 11:02:38 -0800

    Changed Grid Card to have onClick, populated some fictional materials

--- src/organisms/CategoryPage.jsx
+++ src/organisms/CategoryPage.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import bgImg from "../images/batteries.jpg";
+import CategoryGridCard from "../molecules/GridCard";
+import placeholderImg from "../images/category_placeholder.svg";
 
 const Root = styled.div``;
 
+const HeaderTitle = styled.h2`
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0;
+  padding: 0;
+  font-weight: bold;
+  color: #404040;
+  font-size: 36px;
+`;
+
+const MaterialGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+  gap: 33px;
+  margin: 39px 24px;
+`;
+
 const CategoryPage = () => {
+  const [materials, setMaterials] = useState([]);
   //Setup for future requests from backend.
   const { id } = useParams();
+
+  //Temporary population of materials list.
+  useEffect(() => {
+    const newMaterials = [
+      { name: "Lithium", image: placeholderImg },
+      { name: "Alkaline", image: placeholderImg },
+      { name: "Laptop", image: placeholderImg }
+    ];
+    setMaterials(newMaterials);
+  }, []);
 
   //Getting image url from backend.
   //Temporary image imported.
@@ -21,23 +53,16 @@ const CategoryPage = () => {
     position: relative;
   `;
 
-  const HeaderTitle = styled.h2`
-    position: absolute;
-    top: 45%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    margin: 0;
-    padding: 0;
-    font-weight: bold;
-    color: #404040;
-    font-size: 36px;
-  `;
-
   return (
     <Root>
       <Header>
         <HeaderTitle>Batteries</HeaderTitle>
       </Header>
+      <MaterialGrid>
+        {materials.map((mat, key) => (
+          <CategoryGridCard image={mat.image} name={mat.name} key={key} />
+        ))}
+      </MaterialGrid>
     </Root>
   );
 };
