58e5558ecc47fa3b0e1a17da725c729543ad22c8
Mark Artishuk <markyshuk@gmail.com>
2020-01-30 10:44:34 -0800

    Setting up apollo client

--- src/App.js
+++ src/App.js
@@ -5,23 +5,30 @@ import CategoryPage from "./organisms/CategoryPage";
 import { Switch, Route } from "react-router-dom";
 import NavBar from "./molecules/NavBar";
 import MaterialPage from "./organisms/MaterialPage";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 
+const client = new ApolloClient({
+  uri: "https://trashpanda-be.herokuapp.com"
+});
 const App = () => {
   return (
-    <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route exact path="/category/:id">
-          <CategoryPage />
-        </Route>
-        <Route exact path="/category/:id/:materialId">
-          <MaterialPage />
-        </Route>
-      </Switch>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/category/:id">
+            <CategoryPage />
+          </Route>
+          <Route exact path="/category/:id/:materialId">
+            <MaterialPage />
+          </Route>
+        </Switch>
+      </div>
+    </ApolloProvider>
   );
 };
 
