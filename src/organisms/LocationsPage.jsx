ebd4b321b4eba4fe852d0814f4e01d9862469325
Mark Artishuk <markyshuk@gmail.com>
2020-02-03 10:53:59 -0800

    Added basis for locations page and added more Fonts (for figma design)

---
+++ src/organisms/LocationsPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Blurb = styled.h2`
+  font-family: Muli;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  text-align: center;
+`;
+
+const LocationsPage = () => {
+  return <Container></Container>;
+};
+
+export default LocationsPage;
