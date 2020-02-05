f0b6ef28524d56c6dc6fafa02dd661e6dd8b3637
Mark Artishuk <markyshuk@gmail.com>
2020-02-05 09:47:20 -0800

    Nav bar now removes arrow and home buttons when on home page (figma design)

--- src/molecules/BottomNav.jsx
+++ src/molecules/BottomNav.jsx
@@ -34,23 +34,25 @@ const InnerContainer = styled.div`
   position: relative;
 `;
 
-const CameraButton = styled.button`
-  outline: none;
-  border: none;
+function getCameraButton(isHome) {
+  return styled.button`
+    outline: none;
+    border: none;
 
-  background: #404040;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  padding: 16px;
-  border-radius: 100px;
+    background: #404040;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    padding: 16px;
+    border-radius: 100px;
 
-  position: absolute;
-  bottom: 0px;
-  left: 50%;
+    position: absolute;
+    bottom: 0px;
+    left: 50%;
 
-  transform: translate(-50%, -15%);
+    transform: translate(-50%, ${isHome ? `6` : `-15`}%);
 
-  cursor: pointer;
-`;
+    cursor: pointer;
+  `;
+}
 
 const CameraImage = styled.img``;
 
@@ -66,18 +68,20 @@ const BottomNav = () => {
   const handleBackClick = () => {
     history.goBack();
   };
-
+  const CameraButton = getCameraButton(isHome);
   return (
     <Container>
       <InnerContainer>
-        <Img onClick={handleBackClick} src={arrowImg} />
-
         <CameraButton>
           <CameraImage src={cameraImg} alt="camera" />
         </CameraButton>
-        <Link to="/">
-          <Img src={homeImg} />
-        </Link>
+        {isHome ? null : <Img onClick={handleBackClick} src={arrowImg} />}
+
+        {isHome ? null : (
+          <Link to="/">
+            <Img src={homeImg} />
+          </Link>
+        )}
       </InnerContainer>
     </Container>
   );
