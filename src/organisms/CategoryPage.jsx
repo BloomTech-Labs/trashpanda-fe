63f958b59f33d6534ce4e48ab37ab1870c01314d
Mark Artishuk <markyshuk@gmail.com>
2020-02-05 11:56:57 -0800

    Redesigned header for category page and grid card placement

--- src/organisms/CategoryPage.jsx
+++ src/organisms/CategoryPage.jsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useParams, useHistory } from "react-router-dom";
-import bgImg from "../images/batteries.jpg";
 import CategoryGridCard from "../molecules/GridCard";
 import placeholderImg from "../images/category_placeholder.svg";
 
 const Root = styled.div``;
 
 const HeaderTitle = styled.h2`
-  position: absolute;
-  top: 45%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  margin: 0;
-  padding: 0;
+  font-family: Muli;
+  font-style: normal;
   font-weight: bold;
-  color: #404040;
-  font-size: 36px;
+  font-size: 24px;
+  line-height: 30px;
+  text-align: center;
+
+  color: #000000;
+
+  margin: 20px 0px 39px 0px;
+  padding: 0;
 `;
 
 const MaterialGrid = styled.div`
@@ -60,27 +61,13 @@ const CategoryPage = ({ categorylist, materiallist }) => {
     }
   }, [currentFamily]);
 
-  //Getting image url from backend.
-  //Temporary image imported.
-  const Header = styled.div`
-    background-image: url(${bgImg});
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    width: 100%;
-    height: 214px;
-    position: relative;
-  `;
-
   const onClick = materialId => {
     history.push(`/material/${materialId}`);
   };
 
   return (
     <Root>
-      <Header>
-        <HeaderTitle>{currentFamily.description}</HeaderTitle>
-      </Header>
+      <HeaderTitle>{currentFamily.description}</HeaderTitle>
       <MaterialGrid>
         {materials.map((mat, key) => (
           <CategoryGridCard
