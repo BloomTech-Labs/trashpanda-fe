ae1afd5082182b088705bb7c4bed2d9721a8f69a
Mark Artishuk <markyshuk@gmail.com>
2020-02-06 08:53:24 -0800

    Removed unused images andn added base for permissions/landing introduction page

---
+++ src/organisms/LandingPage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Title = styled.h2`
+  font-family: Muli;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 30px;
+
+  color: #404040;
+`;
+
+const PText = styled.p`
+  font-family: Muli;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 23px;
+
+  color: #404040;
+`;
+
+const LandingPage = () => {
+  return <Container></Container>;
+};
+
+export default LandingPage;
---
+++ src/organisms/PermissionPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const PermissionPage = () => {
+  return <Container></Container>;
+};
+
+export default PermissionPage;
