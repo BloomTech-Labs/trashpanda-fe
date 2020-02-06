e69e4c8a3e7b3dedd92e955870af0fcf4c26c1e1
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-02-06 14:30:18 -0500

    Search Functionality now filters the list of items and sets the search term to the selected filtered material. The form's styling also changes upon the searchTerm being populated

--- src/molecules/HomeSearchBar.jsx
+++ src/molecules/HomeSearchBar.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import lensImg from "../images/lens.svg";
 
-const SearchContainer = styled.div`
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import { useHistory } from "react-router-dom";
+
+const SearchPageContainer = styled.div`
+  position: relative;
+  z-index: 1;
+  align-items: center;
+  margin-top: 0;
+  width: 100%;
+  /* border: 1px solid black;
+  border-radius: 20px; */
+  /* box-sizing: border-box;  */
+`;
+const SearchContainer = styled.form`
   display: flex;
   align-items: center;
   border: 1px solid black;
   width: 100%;
   box-sizing: border-box;
-  border-radius: 20px;
+  /* border-radius: 20px; */
+  border-radius: ${props =>
+    props.searchTerm !== "" ? "20px 20px 0 0" : "20px"};
   height: 36px;
   font-size: 18px;
   margin-top: 20px;
@@ -20,30 +36,113 @@ const InputField = styled.input`
   color: #847d7d;
   font-size: 18px;
   line-height: 25px;
-  width: 100%;
+  width: inherit;
   margin-right: 10px;
   /* identical to box height */
 `;
-
 const Img = styled.img`
   height: 24px;
   margin-left: 14px;
   margin-right: 14px;
 `;
+const DropList = styled.ul`
+  position: absolute;
+  border: 1px solid black;
+  border-top: none;
+  width: inherit;
+  list-style-type: none;
+  text-align: none;
+  margin: 0;
+  padding: 0;
+  background-color: white;
+  box-sizing: border-box;
+  border-radius: 0 0 20px 20px;
+`;
+const DropListItem = styled.li`
+  margin: 0;
+  padding: 10px 0;
+  width: 100%;
+  margin-left: 9%;
+`;
 
+const GET_MATERIALS = gql`
+  query getAllMaterials {
+    materials {
+      description
+      material_id
+      # long_description
+    }
+  }
+`;
 const HomeSearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [filtered, setFiltered] = useState([]);
+  const { loading, data } = useQuery(GET_MATERIALS);
+  const history = useHistory();
+
+  // gets the materials and filters the material to the search term
+  useEffect(() => {
+    if (data && !loading) {
+      const newList = data.materials.filter(mat =>
+        mat.description.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFiltered(newList);
+    }
+  }, [searchTerm]);
+
+  // sets the selected suggestion to be the search term
+  const suggestionSelected = value => {
+    setSearchTerm(value);
+  };
+
+  // displays the filtered suggestions
+  const renderFiltered = () => {
+    if (searchTerm === "") {
+      return null;
+    } else {
+      return (
+        <DropList>
+          {filtered.map(mat => (
+            <DropListItem
+              key={mat.material_id}
+              onClick={() => suggestionSelected(mat.description)}
+            >
+              {mat.description}
+            </DropListItem>
+          ))}
+        </DropList>
+      );
+    }
+  };
+
+  // const onEnteredText = () => {
+  //   if (filtered > 0) {
+  //     element.classList.add("dropdown");
+  //   }
+  // };
+
+  // submits and continues to the material page.
+  const handleSubmit = e => {
+    e.preventDefault();
+    const foundMaterial = filtered.find(mat => {
+      return searchTerm.toLowerCase() == mat.description.toLowerCase();
+    });
+    history.push(`/material/${foundMaterial.material_id}`);
+  };
+
   return (
-    <SearchContainer>
-      <Img src={lensImg} alt="lens" />
-      <InputField
-        type="text"
-        placeholder="enter search term"
-        value={searchTerm}
-        onChange={e => setSearchTerm(e.target.value)}
-      />
-    </SearchContainer>
+    <SearchPageContainer>
+      <SearchContainer onSubmit={handleSubmit} searchTerm={searchTerm}>
+        <Img src={lensImg} alt="lens" />
+        <InputField
+          type="text"
+          placeholder="enter search term"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+      </SearchContainer>
+      {renderFiltered()}
+    </SearchPageContainer>
   );
 };
-
 export default HomeSearchBar;
