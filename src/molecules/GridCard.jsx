7d5cc89bdee1d977cb7eb02fcd0625504df963a5
Mark Artishuk <markyshuk@gmail.com>
2020-02-11 09:10:11 -0800

    Fixed styled component dynamic rendering warnings

--- src/molecules/GridCard.jsx
+++ src/molecules/GridCard.jsx
@@ -14,29 +14,31 @@ const Img = styled.img`
   width: 100px;
 `;
 
-const CategoryGridCard = ({ image, name, onClick, bold }) => {
-  const Name = styled.p`
-    font-family: Muli;
-    text-align: center;
-    font-style: normal;
-    font-weight: normal;
-    font-size: 18px;
-    line-height: 23px;
-    margin: 0;
-    margin-top: 9px;
-    padding: 0;
-    color: #737373;
-    ${bold
-      ? `font-weight: bold;
-      color: black;
-      `
-      : ""}
-  `;
+const Name = styled.p`
+  font-family: Muli;
+  text-align: center;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 23px;
+  margin: 0;
+  margin-top: 9px;
+  padding: 0;
+  color: #737373;
 
+  ${props =>
+    props.bold
+      ? `font-weight: bold;
+        color: black;
+        `
+      : ""}
+`;
+
+const CategoryGridCard = ({ image, name, onClick, bold }) => {
   return (
     <Container onClick={onClick}>
       <Img src={image} alt={`${name} category`} />
-      <Name>{name}</Name>
+      <Name bold={bold}>{name}</Name>
     </Container>
   );
 };
