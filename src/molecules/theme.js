382fc03444023393f31c994b8cb4080a294acd04
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-02-14 12:27:35 -0500

    adds a global styles file and a theme file to allow the ability to switch between themes. Currently only trying on a few pieces of the app before broadening the styles

---
+++ src/molecules/theme.js
@@ -0,0 +1,13 @@
+// themes for light mode and dark mode
+
+export const lightTheme = {
+  titleText: "#000000",
+  text: "#404040",
+  body: "#FFFFFF"
+};
+
+export const darkTheme = {
+  titleText: "#FFFFFF",
+  text: "#FFFFFF",
+  body: "#1b1b1b"
+};
