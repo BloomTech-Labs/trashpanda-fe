cd5178be8b59617e4f3f036b5ef7fa4c072ace96
CJ Lucido <c.jose.lucido@gmail.com>
2020-02-17 15:04:38 -0800

    fixed camera page button to center, divided bottom band into 4 parts, lifted camera page button to higher z index

--- src/molecules/BottomNav.jsx
+++ src/molecules/BottomNav.jsx
@@ -10,13 +10,41 @@ const Container = styled.div`
   width: 100vw;
   height: 56px;
 
-  background: ${({ theme }) => theme.bottomNav};
   position: fixed;
   bottom: 0;
 
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+
+const BottomBandSquare = styled.div`
+  width: 40vw;
+  height: 56px;
+
+  background: ${({ theme }) => theme.bottomNav};
+  position: fixed;
+  bottom: 0;
+`;
+
+const BottomBandLeft = styled.div`
+  left: 39vw;
+  width: 11vw;
+  height: 56px;
+
+  background: ${({ theme }) => theme.bottomNav};
+  position: fixed;
+  bottom: 0;
+`;
+
+const BottomBandRight = styled.div`
+  right: 39vw;
+  width: 11vw;
+  height: 56px;
+
+  background: ${({ theme }) => theme.bottomNav};
+  position: fixed;
+  bottom: 0;
 `;
 
 const Img = styled.img`
@@ -42,9 +70,9 @@ const CameraButton = styled.button`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   padding: 16px;
   border-radius: 100px;
-
-  position: absolute;
-  bottom: 0px;
+  z-index: 2;
+  position: fixed;
+  bottom: 17px;
   left: 50%;
 
   transform: translate(-50%, ${props => (props.isHome ? `6` : `-15`)}%);
@@ -74,9 +102,13 @@ const BottomNav = () => {
   return (
     <Container>
       <InnerContainer>
+        <BottomBandSquare style={{ left: 0 }} />
+        <BottomBandLeft />
         <CameraButton onClick={handleCameraBtn}>
           <CameraImage src={cameraImg} alt="camera" />
         </CameraButton>
+        <BottomBandRight />
+        <BottomBandSquare style={{ right: 0 }} />
         {isHome ? null : <Img onClick={handleBackClick} src={arrowImg} />}
 
         {isHome ? null : (
