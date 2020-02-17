2a306b32f33b1f97fa9a7a6b911e81ea4238cf2a
Mark Halls <mark.halls@gmail.com>
2020-02-17 16:21:51 -0700

    Add workbox service worker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import AppCache from "./AppCache";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <AppCache>
@@ -14,8 +13,3 @@ ReactDOM.render(
   </AppCache>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
