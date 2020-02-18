9c2436eb124971dc42ae7f0aafeacbaab9174acc
CJ Lucido <c.jose.lucido@gmail.com>
2020-02-18 13:30:04 -0800

    hover allows for results pop up on desktop, will pop up if clicked on mobile view in dev tools (hack)

--- src/molecules/ResultsTab.jsx
+++ src/molecules/ResultsTab.jsx
@@ -7,12 +7,12 @@ import { useHistory } from "react-router-dom";
 const Container = styled.div`
     max-width= 575px;
     width: 100%;
-    position: fixed;
+    position:absolute;
     bottom: 0;
     display: flex;
     justify-content: space-evenly;
     align-items: center;
-    
+
 `;
 
 const ResultsBox = styled.div`
@@ -22,7 +22,12 @@ const ResultsBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border-radius: 15px 15px 0px;
+  border-radius: 15px 15px 0px 0px;
+  transition: height 4s;
+
+  &:hover {
+    height: 500px;
+  }
 `;
 
 const ResultsTab = () => {
@@ -37,7 +42,7 @@ const ResultsTab = () => {
     <Container>
       {result === "" ? (
         <ResultsBox>
-          {" "}
+          <p>No results found</p>
           <p>1</p>
           <p>2</p>
           <p>3</p>
@@ -45,7 +50,8 @@ const ResultsTab = () => {
         </ResultsBox>
       ) : (
         <ResultsBox>
-          {" "}
+          <p>Is this an</p>
+          {result}
           <p>1</p>
           <p>2</p>
           <p>3</p>
