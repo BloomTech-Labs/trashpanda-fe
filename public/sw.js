02037af51326aa6780bffd34ffbfac191bde8090
Mark Halls <mark.halls@gmail.com>
2020-02-18 14:46:14 -0700

    Remove logs

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,6 @@ importScripts(
 );
 
 if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-
   workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
--- src/index.js
+++ src/index.js
@@ -13,16 +13,3 @@ ReactDOM.render(
   </AppCache>,
   document.getElementById("root")
 );
-
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker
-//       .register("sw.js")
-//       .then(registration => {
-//         console.log("SW registered: ", registration);
-//       })
-//       .catch(registrationError => {
-//         console.log("SW registration failed: ", registrationError);
-//       });
-//   });
-// }
