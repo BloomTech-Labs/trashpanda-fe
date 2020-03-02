142292d121467386af24e15218376d2ea430c3e7
Mark Halls <mark.halls@gmail.com>
2020-03-01 20:38:40 -0700

    Fix svg color from theme

--- src/molecules/TopNav.jsx
+++ src/molecules/TopNav.jsx
@@ -21,23 +21,20 @@ const NavBar = styled.div`
 const Title = styled.h1`
   font-family: Muli;
   font-size: 20px;
-  color: ${({ theme }) => (theme.name === "Light" ? "#404040" : "#FFFFFF")};
   margin: 0;
+  color: ${({ theme }) => (theme.name === "Light" ? "#404040" : "#FFFFFF")};
 `;
 
+const Svg = styled.svg`
+  fill: ${({ theme }) => (theme.name === "Light" ? "#404040" : "#FFFFFF")};
+`;
 const HomeIcon = ({ onClick, theme }) => {
-  const color = theme.name === "Light" ? "#404040" : "white";
   return (
     <div onClick={onClick}>
-      <svg
-        width="22"
-        height="24"
-        xmlns="http://www.w3.org/2000/svg"
-        fill={color}
-      >
+      <Svg width="22" height="24" xmlns="http://www.w3.org/2000/svg">
         <path d="M13.852 15.325h-5.94a.882.882 0 00-.882.882V24h1.764v-6.91h4.176V24h1.764v-7.793a.882.882 0 00-.882-.882z" />
         <path d="M11.465.222l9.998 8.851c.19.167.298.408.298.66v13.38a.882.882 0 01-.883.882h-5.991c-.975 0-1.764-.79-1.764-1.764h6.873v-12.1L10.88 2.06l-9.116 8.07v12.1h7.047v1.764H.882A.882.882 0 010 23.113V9.733c0-.252.108-.493.297-.66L10.295.222a.882.882 0 011.17 0z" />
-      </svg>
+      </Svg>
     </div>
   );
 };
@@ -47,15 +44,14 @@ const BackArrowIcon = ({ onClick, theme }) => {
 
   return (
     <div onClick={onClick}>
-      <svg
+      <Svg
         width="22"
         height="20"
         viewBox="0 0 22 20"
-        fill={color}
         xmlns="http://www.w3.org/2000/svg"
       >
         <path d="M22 9.54545H2.69388L10.8922 1.01372L9.87755 0L0 10L9.87755 20L10.8788 18.9872L2.69388 10.9091H22V9.54545Z" />
-      </svg>
+      </Svg>
     </div>
   );
 };
@@ -85,13 +81,13 @@ const TopNav = ({ theme, toggleTheme, setShutterPress, shutterPress }) => {
         </Route>
 
         <Route exact path="/camera">
-          <BackArrowIcon onClick={() => handleCameraBackClick()} />
-          <HomeIcon onClick={handleHome} />
+          <BackArrowIcon theme={theme} onClick={handleCameraBackClick} />
+          <HomeIcon theme={theme} onClick={handleHome} />
         </Route>
 
         <Route>
-          <BackArrowIcon onClick={handleBackClick} />
-          <HomeIcon onClick={handleHome} />
+          <BackArrowIcon theme={theme} onClick={handleBackClick} />
+          <HomeIcon theme={theme} onClick={handleHome} />
         </Route>
       </Switch>
     </NavBar>
