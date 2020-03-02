0120de5133322f370dabe9f6dfe07d4e09df833f
Mark Halls <mark.halls@gmail.com>
2020-03-02 00:55:36 -0700

    Fix url

--- src/organisms/ClusterPage.jsx
+++ src/organisms/ClusterPage.jsx
@@ -34,7 +34,7 @@ const ClusterPage = ({ appCluster }) => {
   const history = useHistory();
 
   if (appCluster && appCluster.materials.length === 1) {
-    history.push(`/material/${appCluster.materials[0]}.material_id`);
+    history.push(`/material/${appCluster.materials[0].material_id}`);
   }
 
   const onClick = materialId => {
